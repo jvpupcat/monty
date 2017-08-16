873b106cb9e07ed75fa15c2272c922aca1f92497
wendysegura <118@holbertonschool.com>
2017-08-16 17:06:54 +0000

    upload

--- monty.h
+++ monty.h
@@ -2,6 +2,25 @@
 #define MONTY_H
 #define EXIT_FAILURE 1
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define TRUE 1
+/* error strings */
+#define FAIL "EXIT_FAILURE"
+#define ERR_ARG "USAGE: monty file\n"
+#define ERR_OPEN "Error: Can't open file\n"
+#define ERR_INVALID "unknown instruction\n"
+#define ERR_INT "usage: push integer\n"
+#define ERR_PINT "can't pint, stack empty\n"
+#define ERR_POP "can't pop an empty stack\n"
+#define ERR_SWAP "can't swap, stack too short\n"
+#define ERR_ADD "can't add, stack too short\n"
+
+extern int **tracker;
 
 /* HELPER FUNCTIONS */
 int _strcmp(char *s1, char *s2);
