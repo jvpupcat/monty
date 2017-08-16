3be063858800b03bfa529b02d61c987727fafcdc
jvpupcat <vuong.joann@yahoo.com>
2017-08-16 03:17:00 +0000

    update first commit

---
+++ pushpallpintpop.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+
+/**
+ * push - function that pushes an element to the stack
+ * @stack: stack
+ * @line_number: 
+ *
+ **/
+char *push(stack_t **stack, unsigned int line_number)
+{
+	
+}
+
+/**
+ *
+ *
+ *
+ **/
+char *pall(stack_t **stack, unsigned int line_number)
+{
+
+}
+
+/**
+ *
+ *
+ *
+ **/
+char *pop(stack_t **stack, unsigned int line_number)
+{
+
+}
---
+++ swapaddnop.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+/**
+ *
+ *
+ *
+ **/
+char *swap(stack_t **stack, unsigned int line_number)
+{
+
+}
+
+/**
+ *
+ *
+ *
+ **/
+char *_add(stack_t **stack, unsigned int line_number)
+{
+
+}
+
+/**
+ *
+ *
+ *
+ **/
+char *nop(stack_t **stack, unsigned int line_number)
+{
+	return (NULL);
+}
