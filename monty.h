d4d35b866f6ab972591c72ec8da387b132e9cefe
wendysegura <118@holbertonschool.com>
2017-08-16 16:36:27 +0000

    seperated structs from prototypes

--- monty.h
+++ monty.h
@@ -6,36 +6,8 @@
 /* HELPER FUNCTIONS */
 int _strcmp(char *s1, char *s2);
 
-/**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
- */
-typedef struct stack_s
-{
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
-} stack_t;
 
 /* MATCH_OP FUNCTION */
-/**
- * struct instruction_s - opcoode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
- */
-typedef struct instruction_s
-{
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
 int match_op(char *opcode, stack_t **stack, unsigned int line_number);
 void *create_node(int n, stack_t **stack, unsigned int line_number);
 void *push(stack_t **stack, unsigned int line_number);
