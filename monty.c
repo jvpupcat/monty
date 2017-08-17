d04c5f0ca0105c83db48e76794618e97f6390b08
wendysegura <118@holbertonschool.com>
2017-08-17 19:40:17 +0000

    upload

--- monty.c
+++ monty.c
@@ -16,31 +16,30 @@ int main(int argc, char *argv[])
 	char *tokens, *line = NULL, *num_stack;
 	stack_t *stack = NULL;
 	(void) argc;
-	/**if (argc != 2)
+	
+	if (argc != 2)
 	{
 		perror("USAGE: monty file");
 		exit(EXIT_FAILURE);
 	}
+
+	filename = fopen(argv[1], "r");	
 	if (filename == NULL)
 	{
 		perror("Error: Can't open file");
 		exit(EXIT_FAILURE);
-	}**/
-	filename = fopen(argv[1], "r");
+	}
+	
 	while ((read = getline(&line, &len, filename)) != -1)
 	{
 		line_number++;
-		/**printf("Read: %ld\n", read);
-		printf("Line: %s\n", line);
-		printf("Len: %ld\n", len);
-		continue;**/
 		tokens = strtok(line, DELIM);
+		
 		if (strncmp(tokens, "push", strlen("push")) == 0)
 		{
 			num_stack = strtok(NULL, DELIM);
 			printf("num_stack: %s\n", num_stack);
 			push(&stack, line_number, num_stack);
-			printf("num_stack2: %s\n", num_stack);
 		}
 		else
 			match_op(tokens, &stack, line_number);
