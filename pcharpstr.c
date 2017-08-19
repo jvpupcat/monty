ed2d02b8463276eabbf71ba6c7284320b3c66040
jvpupcat <vuong.joann@yahoo.com>
2017-08-19 22:43:09 +0000

    moved a line in pchar

--- pcharpstr.c
+++ pcharpstr.c
@@ -11,13 +11,12 @@ void pchar(stack_t **stack, unsigned int line_number)
 	int node;
 	(void) line_number;
 
-	node = (*stack)->n;
-
 	if (*stack == NULL || stack == NULL)
 	{
 		printf("L%d: %s", line_number, ERR_EMPPCHAR);
 		exit(EXIT_FAILURE);
 	}
+	node = (*stack)->n;
 	if (node >= 'a' && node <= 'z')
 	{
 		putchar(node);
